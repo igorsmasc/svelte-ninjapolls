5911c1f53bb0b720f8b3f4ea42053a1b5bf87d6a
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-07-15 02:23:36 -0300

    Custom Form Validation

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,14 +1,37 @@
 <script>
   import Button from "../shared/Button.svelte";
 
-  let fields = {
-    question: "",
-    answerA: "",
-    answerB: ""
-  };
+  let fields = { question: "", answerA: "", answerB: "" };
+  let errors = { question: "", answerA: "", answerB: "" };
+  let valid = false;
 
   const submitHandler = () => {
-    console.log(fields);
+    valid = true;
+
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long";
+    } else {
+      errors.question = "";
+    }
+
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A cannot be emply";
+    } else {
+      errors.answerA = "";
+    }
+
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B cannot be emply";
+    } else {
+      errors.answerB = "";
+    }
+
+    if (valid) {
+      //add new poll
+    }
   };
 </script>
 
@@ -31,20 +54,29 @@
     margin: 10px auto;
     text-align: left;
   }
+
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: #d91b42;
+  }
 </style>
 
 <form on:submit|preventDefault={submitHandler}>
   <div class="form-field">
     <label for="question">Poll Question:</label>
     <input type="text" id="question" bind:value={fields.question} />
+    <div class="error">{errors.question}</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA} />
+    <div class="error">{errors.answerA}</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB} />
+    <div class="error">{errors.answerB}</div>
   </div>
   <Button type="secondary" flat>Add Poll</Button>
 </form>
